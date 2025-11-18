fd381a170734e4e59578cafed0059592f8cbacff
surendher-babu <surendherabu1330@gmail.com>
2025-11-18 09:26:59 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ import Card from './components/Card'
+ import Nav from './components/Nav'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Nav/>
+    <Card/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+<h1>This is Card</h1>
+    </div>
+  )
+}
+
+export default Card
